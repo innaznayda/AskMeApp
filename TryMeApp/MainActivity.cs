1cd3dad95c98cfb4da5fbd0f13f3fe1a66bc3313
innaznayda <innaznayda@gmail.com>
2019-11-18 17:20:00 +0200

    change targets and add basic button

--- TryMeApp/MainActivity.cs
+++ TryMeApp/MainActivity.cs
@@ -11,6 +11,14 @@ namespace TryMeApp {
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button myButton = FindViewById<Button>(Resource.Id.myButton);
+            myButton.Click += MyButton_Click;
+        }
+
+        private void MyButton_Click(object sender, System.EventArgs e) {
+            var toast = Toast.MakeText(this,"Button was clicked", ToastLength.Short);
+            toast.Show();
         }
     }
 }
